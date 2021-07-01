05f70400775d37cb01d71ce5fc47a57cb78f3bac
Edy Kantor <edycoleee@gmail.com>
2021-07-01 08:37:12 +0700

    dummy data1

---
+++ dashboard-site/src/pages/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1>Coba</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ dashboard-site/src/util/DummyData.js
@@ -0,0 +1,142 @@
+const TersediaTT = [
+  {
+    No: 1,
+    Kelas: "ICU / HCU / ICCU",
+    Jumlah: 64,
+    Kosong: 46,
+  },
+  {
+    No: 2,
+    Kelas: "KELAS I",
+    Jumlah: 126,
+    Kosong: 94,
+  },
+  {
+    No: 3,
+    Kelas: "KELAS II",
+    Jumlah: 100,
+    Kosong: 69,
+  },
+  {
+    No: 4,
+    Kelas: "KELAS III",
+    Jumlah: 304,
+    Kosong: 36,
+  },
+  {
+    No: 5,
+    Kelas: "KELAS VIP B",
+    Jumlah: 37,
+    Kosong: 28,
+  },
+  {
+    No: 6,
+    Kelas: "KELAS VVIP A",
+    Jumlah: 65,
+    Kosong: 50,
+  },
+  {
+    No: 7,
+    Kelas: "KELAS VVIP B",
+    Jumlah: 4,
+    Kosong: 2,
+  },
+];
+
+const TarifRSPerbub = [
+  {
+    No: 1,
+    "Jenis Pelayanan": "Kelas III",
+    "Tarif Biaya": "Rp. 110.000",
+  },
+  {
+    No: 2,
+    "Jenis Pelayanan": "Kelas II",
+    "Tarif Biaya": "Rp. 300.000",
+  },
+  {
+    No: 3,
+    "Jenis Pelayanan": "Kelas I",
+    "Tarif Biaya": "Rp. 400.000",
+  },
+  {
+    No: 4,
+    "Jenis Pelayanan": "Kelas Utama",
+    "Tarif Biaya": "Rp. 500.000",
+  },
+  {
+    No: 5,
+    "Jenis Pelayanan": "Kelas VIP A",
+    "Tarif Biaya": "Rp. 600.000",
+  },
+  {
+    No: 6,
+    "Jenis Pelayanan": "Kelas VIP B",
+    "Tarif Biaya": "Rp. 700.000",
+  },
+  {
+    No: 7,
+    "Jenis Pelayanan": "Kelas VVIP A",
+    "Tarif Biaya": "Rp. 800.000",
+  },
+  {
+    No: 8,
+    "Jenis Pelayanan": "Kelas VVIP B",
+    "Tarif Biaya": "Rp. 1.000.000",
+  },
+  {
+    No: 9,
+    "Jenis Pelayanan": "Kelas Super VVIP",
+    "Tarif Biaya": "Rp. 1.500.000",
+  },
+  {
+    No: 10,
+    "Jenis Pelayanan": "Administrasi",
+    "Tarif Biaya": "Rp. 25.000",
+  },
+];
+
+const dataLoketRJ = {
+  dataDate: "29-06-2021 08:58:55",
+  jmlTotDaftar: 388,
+  jmlTerlayani: 388,
+  daftarBPJSNonPBI: 226,
+  sudahBPJSNonPBI: 226,
+  daftarBPJSPBI: 129,
+  sudahBPJSPBI: 129,
+  daftarJamkesda: 2,
+  sudahJamkesda: 2,
+  daftarUmum: 31,
+  sudahUmum: 31,
+};
+
+const dtAntriPoli = [
+  {
+    id: "RJ001",
+    poli: "UMUM [POLI1]",
+    total: 3,
+    terlayani: 3,
+    dataDate: "29-06-2021 08:58:55",
+  },
+  {
+    id: "RJ002",
+    poli: "PENYAKIT DALAM [POLI2]",
+    total: 12,
+    terlayani: 7,
+    dataDate: "29-06-2021 08:58:55",
+  },
+  {
+    id: "RJ002",
+    poli: "PENYAKIT DALAM [POLI2]",
+    total: 22,
+    terlayani: 3,
+    dataDate: "29-06-2021 08:58:55",
+  },
+  {
+    id: "RJ006",
+    poli: "MATA [POLI6]",
+    total: 12,
+    terlayani: 4,
+    dataDate: "29-06-2021 08:58:55",
+  },
+];
